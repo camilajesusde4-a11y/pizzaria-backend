ff235e1f5496074ed08ca0b8773570cfff3b09cd
Valente do krlh <camilajesusde4@gmail.com>
2026-04-21 05:13:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,50 +3,45 @@ import fetch from "node-fetch";
 import cors from "cors";
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
-const CLIENT_ID = "COLOQUE_SEU_CLIENT_ID_AQUI";
-const CLIENT_SECRET = "COLOQUE_SEU_CLIENT_SECRET_AQUI";
+const CLIENT_ID = "live_1ef3c07d203af33a5fb0d8b0f5a4f4c9";
+const CLIENT_SECRET = "sk_d0470e88b7fba535b866043d011246f3241fe8611eba65b012f105aa522cfe7b";
 
 app.post("/criar-pix", async (req, res) => {
-  const { valor, nome } = req.body;
-
   try {
+    const { amount, payerName, payerDocument } = req.body;
+
     const response = await fetch("https://api-connectmdcpay.squareweb.app/api/v1/deposit", {
       method: "POST",
       headers: {
-        "x-client-id": live_4f688e7427cbd490a10fbd7222e8076a,
-        "x-client-secret": sk_1538af5b25f64c620ba439d954d3a72ea5964234b7495be10cc4451a2c8cf1c9,
+        "x-client-id": CLIENT_ID,
+        "x-client-secret": CLIENT_SECRET,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        amount: valor,
-        description: "Pedido pizzaria",
-        payerName: nome || "Cliente",
-        payerDocument: "12345678900"
+        amount,
+        description: "Pedido Pizzaria",
+        payerName,
+        payerDocument
       })
     });
 
     const data = await response.json();
 
-    res.json({
-      qr_code: data.copyPaste,
-      qr_code_base64: data.qrcodeUrl.replace("base64:", ""),
-      transactionId: data.transactionId
-    });
+    res.json(data);
 
   } catch (error) {
     console.log(error);
-    res.status(500).json({ erro: "Erro ao gerar Pix" });
+    res.status(500).json({ error: "Erro ao gerar pix" });
   }
 });
 
 app.get("/verificar/:id", async (req, res) => {
-  const id = req.params.id;
-
   try {
+    const { id } = req.params;
+
     const response = await fetch("https://api-connectmdcpay.squareweb.app/api/transactions/check", {
       method: "POST",
       headers: {
@@ -61,17 +56,12 @@ app.get("/verificar/:id", async (req, res) => {
 
     const data = await response.json();
 
-    res.json({
-      status: data.transaction.transactionState
-    });
+    res.json(data);
 
-  } catch (e) {
-    res.status(500).json({ erro: true });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao verificar pagamento" });
   }
 });
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
-});
+app.listen(PORT, () => console.log("Servidor rodando"));
